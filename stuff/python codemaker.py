cf9e8538a8f1e597c42db53f339b42fd823e95b7
Angelocondo <30901486+Angelocondo@users.noreply.github.com>
2017-08-10 10:33:18 -0400

    Add files via upload

---
+++ stuff/python codemaker.py
@@ -0,0 +1,111 @@
+""" Number = 5
+
+Bool = True
+
+Number = 7 #assigning the value 7 to the variable Number
+
+print(Number) """
+
+""" addition = 45 + 43
+
+print(addition)
+
+Subraction = 55 - 44
+
+print(subtraction)
+
+multiplication = 4 * 5
+
+print(multiplication)
+
+division = 20 / 5
+
+print(division)
+
+exponent = 2**3
+
+print(exponent)
+
+modulo = 10 % 3
+
+print(modulo)"""
+
+"""
+Cost of a meal: 25
+Tax:13%
+Tip:15%
+
+Calculate the total cost of the meal after tax and tip have been applied
+
+"""
+
+"""
+a=25*1.13
+b=a*1.15
+print(b)
+
+"""
+"""
+name = "shaad"
+
+name = "Ryan"
+
+print(len(name))
+
+print(name.lower()) #dot notation can only be used on string data types!
+
+print(name.upper())"""
+
+"""
+
+number = 15
+fruit = "apples"
+
+print(str(number)+ fruit)"""
+
+
+name = input("what is your name?")
+colour=input("what is your favourite colour?")
+sentence="my name is" + name "and my favorite colour is "+ colour
+print(sentence)
+
+
+"""
+
+== is equal to
+!= not equal to
+
+< less than
+> more than
+<=less than or equal to
+>= more than or equal to
+
+"""
+
+#1 (20-10) > 15        false
+#2 (10+17)==3**16      false
+#3 (1**2) <= -1        false
+#4 40*4 > -4           true
+#5 100 !=10**2         false
+
+#and
+
+#1(-(-(-2))) == -2 and 4>= 16**0.5         false 
+#2  19%4 != 300/10/10and False             false
+#3 -(1**2)< 2**0 and 10%10 <=20-(10*2)     true
+
+#or
+
+#1    100**0.5 >= 50 or false                           false
+#2    true or false                                     true
+#3    1**100 == 100 **1 or 3*2*1 != 3+2+1               false
+#4    2**3 == 108%100 or "Apple" == "Orange"            true
+
+#not
+
+#1  not 3**4 < 4**3                                     true
+#2  not 10%3 <= 10%2                                    true
+#3  not 3**2 +  4**2  != 5**2                           true
+#4  not not false                                       false
+
+
